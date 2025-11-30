c3a91db02e83a2946b6c4d78d7987da899ef8b03
Mahad <mahadrana054@gmail.com>
2025-11-30 10:27:18 +0500

    Create problems.sql

---
+++ problems.sql
@@ -0,0 +1,31 @@
+```sql
+  1.Marital Status Distribution of Employees
+  
+  2.Employee Salary Classification (Low / Medium / High)
+  
+  3.Employee Count by Job Position
+  
+  4.Employees With Highest Absences
+  
+  5.Low-Performance Employees and Their Recruitment Sources
+  
+  6.Best and Worst Recruitment Sources Based on Performance
+  
+  7.Average Satisfaction Score by Department
+  
+  8.Salary Difference Between Consecutive Employees
+  
+  9.Employees Earning Above Department Average Salary
+  
+  10.Cleaning Invalid Date of Birth Entries
+  
+  11.Low-Performance Employees Above Age 40
+  
+  12.Employees With Absences Above Adjusted Average
+  
+  13.Department-Wise Absences Above Average
+  
+  14.Female Employees by Marital Status
+  
+  15.Salary Increase for Widowed and Divorced Female Employees
+```
